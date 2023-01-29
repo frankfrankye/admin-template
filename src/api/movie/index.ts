c4b81df416859fb6951677cead655d968d291aa3
frankfrankye <yedaf98@gmail.com>
2023-01-29 23:07:05 +0800

    first init

---
+++ src/api/movie/index.ts
@@ -0,0 +1,13 @@
+// import { defHttp } from '/@/utils/http/axios';
+
+// enum Api {
+//   MOVIE_LIST = '/v1/movies',
+// }
+
+// export const getMovieList = (params: any) => defHttp.get<any>({ url: Api.MOVIE_LIST, params });
+
+// export const addMovie = (items: Array<any>) =>
+//   defHttp.post({ url: Api.MOVIE_LIST, params: { items } });
+
+// export const editCurrency = (id: any, params: any) =>
+//   defHttp.put<any>({ url: Api.MOVIE_LIST + '/' + id, params });
